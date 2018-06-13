f03afc07c743a5a2335d5455383b0dff816a2fe4
PPPjm <2416659142@qq.com>
2018-06-13 21:16:09 +0800

    HUAWEI

---
+++ js/new_file.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$("#demo-navbar .dropdown-menu a").click(function(){
+		var href=$(this).attr("href");
+        $("#tab-list a[href='"+href+"']").tab("show");
+	});
+});
